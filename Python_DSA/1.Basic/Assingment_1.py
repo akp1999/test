dab6e282b15241aeef57a455c065d13bf424049b
Ashish Pandey <9454516830a@gmail.com>
2024-01-12 01:38:12 +0530

    wip

--- Python_DSA/1.Basic/Assingment_1.py
+++ Python_DSA/1.Basic/Assingment_1.py
@@ -51,4 +51,7 @@ else:
 x: int = int(input("enter the total no of games played = "))
 y: int = int(input("enter the total no of games won = "))
 z: int = int(input("enter the total no of games loss = "))
+tie :int = x-y-z
+points = ((y*4)+(tie*2))
+points = print("total poins for win =" , points)
 
--- Python_DSA/1.Basic/tempCodeRunnerFile.py
+++ Python_DSA/1.Basic/tempCodeRunnerFile.py
@@ -1,3 +1,6 @@
-side: int = int(input("enter the side details of square = "))
-area: int = side * side
-print("Area of Square = ", area)
+x: int = int(input("enter the total no of games played = "))
+y: int = int(input("enter the total no of games won = "))
+z: int = int(input("enter the total no of games loss = "))
+tie :int = x-y-z
+points = ((y*4)+(tie*2))
+points = print("total poins for win " , points)
